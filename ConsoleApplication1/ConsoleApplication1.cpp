d906e570b280c8f25df03a53e14fdefece2f9fa6
LevGolodin <nidolgolevlevlevlev@mail.ru>
2024-04-05 14:34:35 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+rettretrete
+rtetearghdfgdf
+hreahrgthiorejhi
+j
+y
+jojkrfrstvc
+
+jryjrt
+jrjry
+jr
+tj
+rtjrt
+jrtjrrj
+
